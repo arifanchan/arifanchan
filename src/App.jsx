62960fc071366134d1fee0b5b3c8259fb2ef6b00
Arifa Chan <128233413+arifanchan@users.noreply.github.com>
2024-01-05 23:28:22 +0000

    siteProps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Arifa Chan",
+  title: "Student at Bina Sarana Informatika",
+  email: "19215277@bsi.ac.id",
+  gitHub: "arifanchan",
+  instagram: "arifachan",
+  linkedIn: "arifachan",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "arifachan",
+  youTube: "arifachan",
 };
 
 const primaryColor = "#4E567E";
